b5d12009549e51e9262f914fa115b462784ae43a
aanchal <waliaaanchal02@gmail.com>
2022-02-20 09:48:48 +0530

    added animation

--- pages/components/ProjectHeading.js
+++ pages/components/ProjectHeading.js
@@ -6,19 +6,49 @@ const ProjectHeading = () => {
     const color = useColorModeValue( 'gray.500','white')
 
     let MText = motion(Text)
-    let Mdivider = motion(Divider)
+    let MDivider = motion(Divider)
     let MImage = motion(Image)
   return (<>
     <Flex justify="center" align="end" mt = "10" >
-      <Image src="img-47.png" boxSize="150px" />
-        <Text  fontSize='8xl' color={color}>
+      <MImage src="img-47.png" boxSize="150px" />
+        <MText  fontSize='8xl' color={color}
+        
+        initial = {{
+          opacity : 0
+        }}
+
+        animate={{
+          opacity : 1
+        }}
+        
+
+        transition={{
+          duration : 2
+        }}
+        
+        >
             Projects
-        </Text>
+        </MText>
         
         
     </Flex>
     <Flex justify="center" mb="10">
-    <Divider width="30%" />
+    <MDivider  ml={10}
+
+    initial={{
+      width : "0%"
+     }}
+     
+     animate={{
+       width : "40%"
+     }}
+
+     transition={{
+       duration : 2
+     }}
+     
+     
+     />
     </Flex>
     </>
   )
--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -3,24 +3,87 @@ import { Flex, Box, useColorModeValue, Text, Divider, Button } from '@chakra-ui/
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import {FiBarChart} from "react-icons/fi"
 import {FaGithub} from "react-icons/fa"
+import {motion} from "framer-motion"
+
+
+
 
 const Projects = () => {
+
+
+
+
+
   const bg = useColorModeValue( 'gray.700','gray.900')
   let color = useColorModeValue("gray.200", "gray.100")
-  let btncolor = useColorModeValue("gray.500", "gray.200")
+  let btncolor = useColorModeValue("gray100", "gray.200")
   let border = useColorModeValue("gray.900" ,"gray.100")
+  let MBox = motion(Box)
+  let MDivider = motion(Divider)
+
+
+
+
+
   return (<>
-  <Divider width="57.4vw" ml ="21.3vw" />
+  <MDivider width="57.4vw" ml ="21.3vw"
+  
+
+  initial = {{
+    width : 0
+  }}
+  animate = {{
+     width : "57.4vw"
+  }}
+  transition = {{
+    duration : 1
+  }}
+  />
     <Flex justify="center" >
-      <Divider orientation="vertical" mr="5" height="130vh"/>
+      <MDivider orientation="vertical" mr="5" height="130vh"
+
+     
+initial = {{
+  opacity :0
+}}
+animate={{
+  opacity : 1
+}}
+transition={{
+  duration : 4
+}}
+      
+      
+      />
         <Flex direction="column" mt={5}>
-          <Box
+          <MBox
           width="25vw"
           bg = {bg}
           color ={color}
           boxShadow='dark-lg'
           mr="50"
           height="35vh"
+
+
+          initial ={{
+            width : "0%",
+            opacity : 0
+          }}
+          animate={{
+            width : "25vw",
+            opacity : [0,0, 1]
+          }}
+
+          transition ={{
+            duration : 2
+          }}
+ 
+
+
+
+
+
+
           >
             <Flex fontSize="4xl" direction = "column" align="center" >
                 <Text mt="5">
@@ -35,8 +98,8 @@ const Projects = () => {
                 </Text>
                 <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
             </Flex>
-          </Box>
-          <Box
+          </MBox>
+          <MBox
           width="25vw"
           bg = {bg}
           color ={color}
@@ -44,6 +107,21 @@ const Projects = () => {
         
           height="35vh"
           mt={50}
+
+
+           
+          initial ={{
+            width : "0%",
+            opacity : 0
+          }}
+          animate={{
+            width : "25vw",
+            opacity : [0,0, 1]
+          }}
+
+          transition ={{
+            duration : 2
+          }}
           >
             <Flex fontSize="4xl" direction = "column" align="center" >
                 <Text mt="5">
@@ -58,8 +136,8 @@ const Projects = () => {
                 </Text>
                 <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
             </Flex>
-          </Box>
-           <Box
+          </MBox>
+           <MBox
           width="25vw"
           bg = {bg}
           color ={color}
@@ -67,6 +145,21 @@ const Projects = () => {
           mr="50"
           height="35vh"
           mt={50}
+
+
+          
+          initial ={{
+            width : "0%",
+            opacity : 0
+          }}
+          animate={{
+            width : "25vw",
+            opacity : [0,0, 1]
+          }}
+
+          transition ={{
+            duration : 2
+          }}
           >
             <Flex fontSize="4xl" direction = "column" align="center" >
                 <Text mt="5">
@@ -81,17 +174,32 @@ const Projects = () => {
                 </Text>
                 <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
             </Flex>
-          </Box>
+          </MBox>
         </Flex>
 
         <Flex mt="100" direction="column">
-          <Box
+          <MBox
           width="25vw"
           bg = {bg}
           color ={color}
           boxShadow='dark-lg'
           mr="50"
           height="35vh"
+
+
+          
+          initial ={{
+            width : "0%",
+            opacity : 0
+          }}
+          animate={{
+            width : "25vw",
+            opacity : [0,0, 1]
+          }}
+
+          transition ={{
+            duration : 2
+          }}
           >
             <Flex fontSize="4xl" direction = "column" align="center" >
                 <Text mt="5">
@@ -106,31 +214,8 @@ const Projects = () => {
                 </Text>
                 <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
             </Flex>
-          </Box>
-          <Box
-          width="25vw"
-          bg = {bg}
-          color ={color}
-          boxShadow='dark-lg'
-          mr="50"
-          height="35vh"
-          mt={50}
-          >
-            <Flex fontSize="4xl" direction = "column" align="center" >
-                <Text mt="5">
-                  {"<> Web </>"}
-                </Text>
-                <Divider width="80%" colorScheme={border} />
-                <Text fontSize = "xl" m = "5">
-                    Weather Application
-                </Text>
-                <Text fontSize = "md" mx = "5" width = "70%"textAlign="center" opacity = "0.7">
-                  I made this mini project using reactjs, bootstrap and open weather api.
-                </Text>
-                <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
-            </Flex>
-          </Box>
-           <Box
+          </MBox>
+          <MBox
           width="25vw"
           bg = {bg}
           color ={color}
@@ -138,6 +223,20 @@ const Projects = () => {
           mr="50"
           height="35vh"
           mt={50}
+
+          
+          initial ={{
+            width : "0%",
+            opacity : 0
+          }}
+          animate={{
+            width : "25vw",
+            opacity : [0,0, 1]
+          }}
+
+          transition ={{
+            duration : 2
+          }}
           >
             <Flex fontSize="4xl" direction = "column" align="center" >
                 <Text mt="5">
@@ -152,10 +251,60 @@ const Projects = () => {
                 </Text>
                 <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
             </Flex>
-          </Box>
+          </MBox>
+           <MBox
+          width="25vw"
+          bg = {bg}
+          color ={color}
+          boxShadow='dark-lg'
+          mr="50"
+          height="35vh"
+          mt={50}
+
+          
+          initial ={{
+            width : "0%",
+            opacity : 0
+          }}
+          animate={{
+            width : "25vw",
+            opacity : [0,0, 1]
+          }}
+
+          transition ={{
+            duration : 2
+          }}
+          >
+            <Flex fontSize="4xl" direction = "column" align="center" >
+                <Text mt="5">
+                  {"<> Web </>"}
+                </Text>
+                <Divider width="80%" colorScheme={border} />
+                <Text fontSize = "xl" m = "5">
+                    Weather Application
+                </Text>
+                <Text fontSize = "md" mx = "5" width = "70%"textAlign="center" opacity = "0.7">
+                  I made this mini project using reactjs, bootstrap and open weather api.
+                </Text>
+                <Button rightIcon={<ArrowForwardIcon />} width="120px"  my={35} color = {btncolor} variant = "solid" label = "Explore" >Explore</Button>
+            </Flex>
+          </MBox>
         </Flex>
   
-        <Divider orientation="vertical" ml="5" height="130vh" />
+        <MDivider orientation="vertical" ml="5" height="130vh"
+        
+        initial = {{
+          opacity :0
+        }}
+        animate={{
+          opacity : 1
+        }}
+        transition={{
+          duration : 5
+        }}
+        
+        
+        />
     </Flex>
     <Flex justify="center" align="center" direction="column">
       <Divider width="57.4vw"  />
